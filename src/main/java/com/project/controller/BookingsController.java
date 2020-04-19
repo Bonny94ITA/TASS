e9bf20c0eba70a3ded17c8c5bf8f0295ae04ecb8
Marcellino94 <alberto.guastalla@edu.unito.it>
2020-04-19 15:44:49 +0200

    pulito il file clips

--- src/main/java/com/project/controller/BookingsController.java
+++ src/main/java/com/project/controller/BookingsController.java
@@ -18,8 +18,7 @@ public class BookingsController {
     private IBookingService bookingService;
     @Autowired
     private ISecretSearch secretSearch;
-
-
+    
     @GetMapping("/prova")
     public List<Alternative> prova(@RequestBody Map<String,Object> requestParams)
             throws CLIPSException, IloException, ParseException {
