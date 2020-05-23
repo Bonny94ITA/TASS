2a2d1e082165a0f9ecb6a6c859c97528760834dd
Giuseppe Biondi <giuv360@hotmail.it>
2020-05-23 16:50:39 +0200

    modificato return di secretsearch

--- src/main/java/com/project/controller/SearchController.java
+++ src/main/java/com/project/controller/SearchController.java
@@ -49,6 +49,7 @@ public class SearchController {
         return new ResponseEntity<>(freeRooms, HttpStatus.OK);
     }
 
+    // TODO => i throws devono essere catturati e tornare 500
     @PostMapping("/hotels/secretSearch")
     public ResponseEntity<?> getSecretSearch(@RequestBody Map<String,Object> requestParams)
             throws CLIPSException, IloException, ParseException {
